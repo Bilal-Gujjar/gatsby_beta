8c38befe0749fa0f5859bd9ad557a080baa7fe16
Bilal Gujjar <bilaltahir515@gmail.com>
2020-10-09 00:50:53 +0500

    Update gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,7 +2,7 @@ exports.onCreatePage = async ({ page, actions }) => {
     const { createPage } = actions
   
    
-    if (page.path.match(/^\/product/)) {
+    if (page.path.match(/product/)) {
       
       page.matchPath = "/product/*"
   
